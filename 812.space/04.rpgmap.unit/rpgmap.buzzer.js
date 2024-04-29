095435256468fee4a51ed66b9ece47ce22806517
Brad Henderson <elliotbradly@gmail.com>
2024-04-29 12:58:32 -0400

    update space

---
+++ 812.space/04.rpgmap.unit/rpgmap.buzzer.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createRpgmap = exports.deleteRpgmap = exports.removeRpgmap = exports.writeRpgmap = exports.readRpgmap = exports.updateRpgmap = exports.initRpgmap = void 0;
+var rpgmap_buzz_1 = require("./buz/rpgmap.buzz");
+Object.defineProperty(exports, "initRpgmap", { enumerable: true, get: function () { return rpgmap_buzz_1.initRpgmap; } });
+var rpgmap_buzz_2 = require("./buz/rpgmap.buzz");
+Object.defineProperty(exports, "updateRpgmap", { enumerable: true, get: function () { return rpgmap_buzz_2.updateRpgmap; } });
+var rpgmap_buzz_3 = require("./buz/rpgmap.buzz");
+Object.defineProperty(exports, "readRpgmap", { enumerable: true, get: function () { return rpgmap_buzz_3.readRpgmap; } });
+var rpgmap_buzz_4 = require("./buz/rpgmap.buzz");
+Object.defineProperty(exports, "writeRpgmap", { enumerable: true, get: function () { return rpgmap_buzz_4.writeRpgmap; } });
+var rpgmap_buzz_5 = require("./buz/rpgmap.buzz");
+Object.defineProperty(exports, "removeRpgmap", { enumerable: true, get: function () { return rpgmap_buzz_5.removeRpgmap; } });
+var rpgmap_buzz_6 = require("./buz/rpgmap.buzz");
+Object.defineProperty(exports, "deleteRpgmap", { enumerable: true, get: function () { return rpgmap_buzz_6.deleteRpgmap; } });
+var rpgmap_buzz_7 = require("./buz/rpgmap.buzz");
+Object.defineProperty(exports, "createRpgmap", { enumerable: true, get: function () { return rpgmap_buzz_7.createRpgmap; } });
+//# sourceMappingURL=rpgmap.buzzer.js.map
---
+++ 812.space/04.rpgmap.unit/rpgmap.model.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RpgmapModel = void 0;
+class RpgmapModel {
+}
+exports.RpgmapModel = RpgmapModel;
+//# sourceMappingURL=rpgmap.model.js.map
